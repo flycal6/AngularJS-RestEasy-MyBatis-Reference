6c9a9beddd38d161e21e9c4e7a42f7af16e9bd39
Brian Thomas <brianraythomas@gmail.com>
2018-01-08 14:29:35 -0700

    race pojo created

--- src/main/java/com/learnstack/entities/Race.java
+++ src/main/java/com/learnstack/entities/Race.java
@@ -7,4 +7,35 @@ public class Race {
 	private int id;
 	private String name;
 	private Set<Driver> drivers;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Set<Driver> getDrivers() {
+		return drivers;
+	}
+	public void setDrivers(Set<Driver> drivers) {
+		this.drivers = drivers;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Race [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", drivers=");
+		builder.append(drivers);
+		builder.append("]");
+		return builder.toString();
+	}
+	
 }
