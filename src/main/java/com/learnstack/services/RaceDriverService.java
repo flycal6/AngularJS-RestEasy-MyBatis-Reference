d43ab663d67d23643b388769f29f66d087f95872
Brian Thomas <brianraythomas@gmail.com>
2018-01-10 15:05:05 -0700

    changed adding drivers from PUT to POST

--- src/main/java/com/learnstack/services/RaceDriverService.java
+++ src/main/java/com/learnstack/services/RaceDriverService.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,7 +20,7 @@ import com.learnstack.utils.MyBatisUtil;
 @Path("/race-drivers")
 public class RaceDriverService {
 	
-	@PUT
+	@POST
 	@Path("/{rid}")
 	@Consumes("application/json")
 	public void addDriversToRace(String driverJson, @PathParam("rid") int rid) {
