f3eae52d1d96f26e85e9738878c9bdfc46b9ca77
Brian Thomas <brianraythomas@gmail.com>
2018-01-16 15:12:38 -0700

    mock test of carService.show(), smoke test, implemented

--- WebContent/test/appModuleTests.js
+++ WebContent/test/appModuleTests.js
@@ -1,5 +1,15 @@
 describe('CarComponent', function() {
-    beforeEach(module('appModule'));
+    var carService;
+    beforeEach(function() {
+
+        module('appModule');
+
+        inject(function(_carService_, _$rootScope_, _$httpBackend_) {
+            scope = _$rootScope_;
+            carService = _carService_;
+            $httpBackend = _$httpBackend_;
+        });
+    });
 
     describe('smokeTest', function() {
         it('should pass as a smokeTest', function() {
@@ -9,4 +19,21 @@ describe('CarComponent', function() {
         });
     });
 
+    describe('car show(id)', function() {
+        it('should produce a car', function(done) {
+            $httpBackend.when('GET', 'services/cars/1').respond({
+                "id": "1",
+                "make": "Toyota",
+                "model": "Tacoma"
+            });
+            carService.show(1).then(function(res) {
+                expect(res.data.id).toBe("1");
+                expect(res.data.make).toBe("Toyota");
+                expect(res.data.model).toBe("Tacoma");
+                done();
+            });
+            $httpBackend.flush();
+            scope.$digest();
+        });
+    });
 });
